ff5b716eadef814158edb1d253c5897b382fba0e
Harish9999kumar <97820952+Harish9999kumar@users.noreply.github.com>
2022-09-21 19:15:43 +0530

    test script

---
+++ src/main/java/org/HMS/objectRepository/CommonPage.java
@@ -0,0 +1,24 @@
+package org.HMS.objectRepository;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CommonPage {
+	public CommonPage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);	
+	}
+	//changes done by person1
+	
+	@FindBy (xpath = "//span[@class='username']")
+	private WebElement dropDownForLogout;
+	@FindBy (xpath = "//a[contains(text(),'Log Out')]")
+	private WebElement logout;
+	//business library
+	public void logoutActivity() {
+		dropDownForLogout.click();
+		logout.click();
+	}
+}
